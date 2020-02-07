bc18986d62c1327093780bbf59bde895ef1319b9
Marta Sousa <marta.sousa@gmail.com>
2020-02-07 11:00:32 -0300

    componentWillUnmount implementado

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -9,12 +9,21 @@ export default class Clock extends React.Component {
     }
 
     componentDidMount(){
-        setInterval(() => {
+        let interval_id = setInterval(() => {
             // this.state.timer = new Date().toLocaleTimeString() // já havia sido iniciado, então precisa atualizar o estado
             this.setState({  //chama o método render automaticamente
                 timer: new Date().toLocaleTimeString()
             })
         }, 1000) // a cada 1 segundo 1000ms
+
+        this.setState({
+            ...this.state, //mantém o estado do jeito que estava
+            interval_id: interval_id
+        })
+    }
+
+    componentWillUnmount(){ //pra quando o componente parar de ser usado, para voltar para o estado inicial/ não deixar 'lixo' na memória
+        clearInterval(this.state.interval_id)
     }
 
     render(){
